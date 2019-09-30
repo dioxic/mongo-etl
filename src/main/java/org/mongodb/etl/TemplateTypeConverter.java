6a8a2cdfc592db3e810227d3ddcfaea5b77fab1e
Mark Baker-Munton <dioxic@gmail.com>
2019-09-30 15:23:43 +0100

    server selector test

--- src/main/java/org/mongodb/etl/TemplateTypeConverter.java
+++ src/main/java/org/mongodb/etl/TemplateTypeConverter.java
@@ -3,10 +3,11 @@ package org.mongodb.etl;
 import picocli.CommandLine;
 import uk.dioxic.mgenerate.core.Template;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 class TemplateTypeConverter implements CommandLine.ITypeConverter<Template> {
-        public Template convert(String value) {
+        public Template convert(String value) throws IOException {
             return Template.from(Paths.get(value));
         }
     }
